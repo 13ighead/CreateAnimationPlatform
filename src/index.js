6874d3675230126902d0ba3aeb9d52d0284a6919
Bighead <lixijun@baidu.com>
2016-02-26 20:26:13 +0800

    init project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-
-define(function(require) {
+/*
+ * @file Entrance
+ * @author Bighead
+ */
+define(function (require) {
     var $ = require('jquery');
+    window.$ = $;
     var draggableDom = $('[data-draggable=true]');
+    var sceneDom = $('.cap-scene');
 
     draggableDom.on('mousedown', function (event) {
-        console.log(event);
+
+        var mouseX = event.clientX;
+        var mouseY = event.clientY;
+
+        this.beginMousePosition = {
+            x: mouseX,
+            y: mouseY
+        };
+
+    });
+
+    draggableDom.on('mousemove', function (event) {
+        if (!this.beginMousePosition) {
+            return;
+        }
+        var mouseX = event.clientX;
+        var mouseY = event.clientY;
+        var offsetMousePosition = {
+            x: mouseX - this.beginMousePosition.x,
+            y: mouseY - this.beginMousePosition.y
+        };
+        console.log(offsetMousePosition);
+        // var offsetY = clientY - this.capClientY;
+        // $(this).css({
+        //     transform: 'scale(2) translateX(' + offsetX + 'px) translateY(' + offsetY + 'px)'
+        // });
+    });
+
+    draggableDom.on('mouseup', function (event) {
+        this.beginMousePosition = null;
     });
 
     return {
