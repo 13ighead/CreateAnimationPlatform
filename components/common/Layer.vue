c9a4d0662d0ab92fe94cc616c2c77238a82c781e
lixijun <lixijun900604@gmail.com>
2016-06-06 20:53:51 +0800

    add mouse event

--- components/common/Layer.vue
+++ components/common/Layer.vue
@@ -3,6 +3,7 @@
         border: 1px solid black;
         background-color: #eeff00;
         position: absolute;
+        user-select: none;
     }
     .origin-style {
         position: absolute;
@@ -19,11 +20,22 @@
         top: 0;
         pointer-events: none;
     }
+    .static {
+        pointer-events: none;
+    }
 </style>
 
 <template>
-    <div class='layer' :style='style' @click='click'>
+    <cap-layer-container
+        class='layer'
+        :style='style'
+        @click='click'
+        @mousemove='drag'
+        @mousedown='dragStart'
+        @mouseup='dragEnd'>
         <div class='label'>
+            lid: {{layer.lid}}
+            <br>
             x: {{layer.measure.x}}
             y: {{layer.measure.y}}
             width: {{layer.measure.width}}
@@ -32,12 +44,14 @@
             scale: {{layer.measure.scale.x}} {{layer.measure.scale.y}}
         </div>
         <div class='origin-style' :style='originStyle'></div>
-    </div>
+    </cap-layer-container>
 
 </template>
 
 <script>
-import { setClid } from '../../models/actions';
+import * as actions from '../../models/actions';
+import { getLayer } from '../../models/getters';
+
 export default {
     name: 'CapLayer',
     ready () {
@@ -50,8 +64,8 @@ export default {
                 width: `${this.layer.measure.width}px`,
                 height: `${this.layer.measure.height}px`,
                 transformOrigin: `${this.layer.measure.origin.x}px ${this.layer.measure.origin.y}px`,
-                transform: `translateX(${this.layer.measure.x}px)
-                            translateY(${this.layer.measure.y}px)
+                transform: `translateX(${this.x}px)
+                            translateY(${this.y}px)
                             rotateX(${this.layer.measure.rotate.x}deg)
                             rotateY(${this.layer.measure.rotate.y}deg)
                             rotateZ(${this.layer.measure.rotate.z}deg)
@@ -70,16 +84,71 @@ export default {
             get () {
                 return this.layer.lid;
             }
+        },
+        x: {
+            get () {
+                let layers = this.layers;
+                return layers[this.lid].measure.x;
+            },
+            set (value) {
+                this.setMeasureX(value);
+            }
+        },
+        y: {
+            get () {
+                let layers = this.layers;
+                return layers[this.lid].measure.y;
+            },
+            set (value) {
+                this.setMeasureY(value);
+            }
         }
     },
     vuex: {
+        getters: {
+            layers: (state) => state.layers,
+            clid: (state) => state.clid
+        },
         actions: {
-            setClid
+            setClid: actions.setClid,
+            setMeasureX: actions.setMeasureX,
+            setMeasureY: actions.setMeasureY
+        },
+        computed: {
+
         }
     },
     methods: {
         click () {
             this.setClid(this.lid);
+        },
+        drag () {
+            if (!this.dragStartFlag) {
+                return;
+            }
+            this.offset = {
+                x: event.clientX - this.dragStartPos.x,
+                y: event.clientY - this.dragStartPos.y
+            }
+            console.log(this.offset);
+            this.x = this.domStartPosition.x + this.offset.x;
+            this.y = this.domStartPosition.y + this.offset.y;
+            console.log(this.x);
+        },
+        dragStart () {
+            this.dragStartFlag = true;
+            this.dragStartPos = {
+                x: event.clientX,
+                y: event.clientY
+            };
+
+            this.domStartPosition = {
+                x: this.x,
+                y: this.y
+            };
+        },
+        dragEnd () {
+            this.dragStartFlag = false;
         }
     }
 }
